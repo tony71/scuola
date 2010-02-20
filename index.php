57dd450eb2a21933dc25464a3957ec2a91e9f4c2
tony71 <antonio.lemon.1971@alice.it>
2010-02-20 17:40:09 +0100

    Modifica pagina iniziale per ricerca studente

--- index.php
+++ index.php
@@ -8,6 +8,12 @@ include('include/header.html');
 	<p>Secondo paragrafo</p>
 <form>
 	<fieldset>
+		<legend>Nome o Cognome</legend>
+		<input type="text" name="nominativo" />
+	</fieldset>
+</form>
+<form>
+	<fieldset>
 		<legend>Scuola</legend>
 		<select name="scuola">
 			<option name="tutte">Tutte </option>
@@ -26,6 +32,54 @@ include('include/header.html');
 			<?php
 			require_once('include/db_connection.php');
 			include('include/select_anno.php');
+			?>
+		</select>
+	</fieldset>
+</form>
+<form>
+	<fieldset>
+		<legend>Classe</legend>
+		<select name="classe">
+			<option name="tutte">Tutte </option>
+			<?php
+			require_once('include/db_connection.php');
+			include('include/select_classe.php');
+			?>
+		</select>
+	</fieldset>
+</form>
+<form>
+	<fieldset>
+		<legend>Sezione</legend>
+		<select name="sezione">
+			<option name="tutte">Tutte </option>
+			<?php
+			require_once('include/db_connection.php');
+			include('include/select_sezione.php');
+			?>
+		</select>
+	</fieldset>
+</form>
+<form>
+	<fieldset>
+		<legend>Indirizzo</legend>
+		<select name="indirizzo">
+			<option name="tutti">Tutti </option>
+			<?php
+			require_once('include/db_connection.php');
+			include('include/select_indirizzo.php');
+			?>
+		</select>
+	</fieldset>
+</form>
+<form>
+	<fieldset>
+		<legend>Stato Studente</legend>
+		<select name="stato">
+			<option name="tutti">Tutti </option>
+			<?php
+			require_once('include/db_connection.php');
+			include('include/select_stato_studente.php');
 			?>
 		</select>
 	</fieldset>
