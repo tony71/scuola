98b1c704249ad292ea21b81da701ae893797bd24
tony71 <antonio.lemon.1971@alice.it>
2010-03-01 12:51:37 +0100

    Fattorizzato codice in autenticate()

--- include/db_connection.php
+++ include/db_connection.php
@@ -1,4 +1,11 @@
 <?php
+function autenticate()
+{
+	header('WWW-Authenticate: Basic Realm="Secret Stash"');
+	header('HTTP/1.0 401 Unauthorized');
+	print('You must provide the proper credentials!');
+}
+
 try {
 	if (isset($_SERVER['PHP_AUTH_USER'])
 		and isset($_SERVER['PHP_AUTH_PW'])) {
@@ -10,20 +17,16 @@ try {
 		$conn_string .= 'dbname=' . $dbname;
 		$db = new PDO($conn_string, $user, $pass);
 		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		autenticate();
 	}
 	else {
-		header('WWW-Authenticate: Basic Realm="Secret Stash"');
-		header('HTTP/1.0 401 Unauthorized');
-		print('You must provide the proper credentials!');
 		exit;
 	}
 }
 catch(PDOException $e) {
 	unset($_SERVER['PHP_AUTH_USER']);
 	unset($_SERVER['PHP_AUTH_PW']);
-	header('WWW-Authenticate: Basic Realm="Secret Stash"');
-	header('HTTP/1.0 401 Unauthorized');
-	print('You must provide the proper credentials!');
+	autenticate();
         echo $e->getMessage();
 	echo '<br />Exception code is: ' . $e->getCode();
 	exit;
