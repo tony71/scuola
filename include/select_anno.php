95584663b9a33c013cba095e7276b49d318e5ff8
tony71 <antonio.lemon.1971@alice.it>
2010-03-01 13:55:55 +0100

    Select anno visualizza anno corrente

--- include/select_anno.php
+++ include/select_anno.php
@@ -1,12 +1,19 @@
 <?php
 
 try {
+	$sql = 'select * from calcola_anno_scolastico()';
+	$stm = $db->query($sql);
+	$r = $stm->fetch(PDO::FETCH_BOTH);
+	$anno = $r['calcola_anno_scolastico'];
+
 	$sql = 'select anno from anni_scolastici order by anno desc';
 	$stm = $db->query($sql);
 	while ($r = $stm->fetch(PDO::FETCH_BOTH)) {
 		$option = '<option value="';
 		$option .= $r['anno'];
-		$option .= '">';
+		$option .= '"';
+		$option .= ($r['anno'] == $anno ? ' selected="yes"' : '');
+		$option .= '>';
 		$option .= $r['anno'];
 		$option .= ' </option>';
 		echo $option;
