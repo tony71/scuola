51bc299f87c7d19372746d8f663ee368e544980b
Antonio Limone <antonio.limone@gmail.com>
2010-09-14 23:37:26 +0200

    Aggiunta variabile page_title

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ if (isset($_POST['submitted'])) {
 	header("Location: http://$host$uri/$extra");
 }
 else {
+	$page_title = 'Benvenuti a Scuola';
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> 
