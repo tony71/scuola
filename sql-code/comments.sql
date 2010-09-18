9cb2bf8957abc843fb9bb7d8240537a92a7197a8
Antonio Limone <antonio.limone@gmail.com>
2010-09-19 00:45:52 +0200

    Aggiunte size su alcune colonne di addebiti

--- sql-code/comments.sql
+++ sql-code/comments.sql
@@ -5,3 +5,7 @@ comment on column persone.cognome is E'size="15"';
 comment on column persone.id_famiglia is E'label@it="Famiglia:"\nsize="5"';
 comment on column persone.professione is E'size="16"';
 comment on column persone.data_nascita is E'label@it="Data di Nascita:"';
+
+-- Tabella pagamenti.addebiti
+COMMENT ON COLUMN pagamenti.addebiti.importo IS 'size="8"';
+COMMENT ON COLUMN pagamenti.addebiti.causale IS 'size="20"';
