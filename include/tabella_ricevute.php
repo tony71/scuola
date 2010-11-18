c5b85b42b4189650d9f92327bd31001aa1eff58c
root <root@pico.intranet.michelerua.it>
2010-11-18 09:07:00 +0100

    spostato colonna elimina ricevuta al fondo e corretto bug sul tipo_pagamento (visualizzava sempre contanti)

--- include/tabella_ricevute.php
+++ include/tabella_ricevute.php
@@ -6,7 +6,6 @@ function result_as_table($result, $tableFeatures="", $link)
 	$table .= "<tr>\n";
 	// $table .= '<th align="left"><b>Dettagli</b></th>';
 	$table .= '<th align="left"><b>Stampa</b></th>';
-	$table .= '<th align="left"><b>Elimina</b></th>';
 	/******
 	for ($i = 1; $i < $noFields; $i++) {
 		$meta = $result->getColumnMeta($i);
@@ -18,13 +17,13 @@ function result_as_table($result, $tableFeatures="", $link)
 	$table .= '<th align="left"><a href="'.$link.'&sort=importo">Importo</th>';
 	$table .= '<th align="left"><a href="'.$link.'&sort=data">Data</th>';
 	$table .= '<th align="left">Tipo pagamento</th>';
+	$table .= '<th align="left"><b>Elimina</b></th>';
 	$bg = '#eeeeee';
 	while ($r = $result->fetch(PDO::FETCH_BOTH)) {
 		$bg = ($bg=='#eeeeee' ? '#ffffff' : '#eeeeee');
 		$table .= '<tr bgcolor="' . $bg . '">';
 		// $table .= '<td align="left"><a href="dettagli_ricevuta.php?id_ricevuta=' . $r['id_ricevuta'] . '">Dettagli</a></td>';
 		$table .= '<td align="left"><a href="stampa_ricevuta.php?id_ricevuta=' . $r['id_ricevuta'] . '" target="_blank">Stampa</a></td>';
-		$table .= '<td align="left"><a href="elimina_ricevuta.php?id_ricevuta=' . $r['id_ricevuta'] . '">Elimina</a></td>';
 		/****
 		// foreach ($r as $column) {
 		for ($i = 1; $i < $noFields; ++$i) {
@@ -32,11 +31,12 @@ function result_as_table($result, $tableFeatures="", $link)
 		}
 		****/
 		$tp = $r['tipo_pagamento'];
-		$tp = ($tp = 'c' ? 'contanti' : ($tp = 'a' ? 'assegno' : 'bonifico'));
+		$tp = ($tp == 'c' ? 'contanti' : ($tp == 'a' ? 'assegno' : 'bonifico'));
 		$table .= '<td align="left">'.$r['codice_scuola'].'-'.$r['numero_ricevuta'].'</td>';
 		$table .= '<td align="left">'.$r['importo_totale'].'</td>';
 		$table .= '<td align="left">'.$r['data_ricevuta'].'</td>';
 		$table .= '<td align="left">'.$tp.'</td>';
+		$table .= '<td align="left"><a href="elimina_ricevuta.php?id_ricevuta=' . $r['id_ricevuta'] . '">Elimina</a></td>';
 		$table .= "</tr>\n";
 	}
 	$table .= "</table>\n\n";
