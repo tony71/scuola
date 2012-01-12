01746b9440f9bfe1dafae070697986acaeff92cb
tony71 <antonio.limone@gmail.com>
2012-01-12 13:47:49 +0100

    Gestione inserimento clienti default

---
+++ clienti.php
@@ -0,0 +1,94 @@
+<?php
+
+require_once('include/db_connection.php');
+
+$page_title = 'Clienti Studente';
+include('include/header.html');
+
+if (isset($_GET['matricola'])) {
+	$matricola = $_GET['matricola'];
+}
+elseif (isset($_POST['matricola'])) {
+	$matricola = $_POST['matricola'];
+}
+else {
+	$sql = "select matricola_studente from studenti where id_persona=$id_persona";
+	try {
+		$stm = $db->query($sql);
+		$num = $stm->rowCount();
+		if ($num != 1) {
+			echo '<p class="error">Non sono presenti persone nel DB.</p>';
+			echo '<a href="dettagli_studente.php?matricola=' . $matricola . '">Torna a Dettagli Studente</a>';
+			include('include/footer.html');
+			exit();
+		}
+		$r = $stm->fetch(PDO::FETCH_BOTH);
+		$matricola = $r['matricola_studente'];
+	
+	}
+	catch(PDOException $e) {
+		echo $e->getMessage();
+	}
+}
+
+if (isset($_POST['salva'])) 
+{
+	$sql = 'select salva_clienti(ARRAY[';
+	$first = TRUE;
+	foreach($_POST['id_persona'] as $key => $value) {
+		if ($first == FALSE) {
+			$sql .= ', ';
+		}
+		$sql .= $value;
+		$first = FALSE;
+	}
+	$sql .= "],'" . $matricola . "')";
+	$stm = $db->query($sql);
+}
+
+$sql = "select nome_breve,cognome_breve from studenti where matricola_studente='$matricola'";
+try {
+	$stm = $db->query($sql);
+	$num = $stm->rowCount();
+	if ($num != 1) {
+		echo '<p class="error">Uhm... studente non trovato.</p>';
+		echo '<a href="dettagli_studente.php?matricola=' . $matricola . '">Torna a Dettagli Studente</a>';
+		include('include/footer.html');
+		exit();
+	}
+	$r = $stm->fetch(PDO::FETCH_BOTH);
+	echo '<h1>Clienti di ' . $r['nome_breve'] . ' ' . $r['cognome_breve'] . '</h1>';
+	
+	$sql = "select * from cerca_clienti('$matricola')";
+	$stm = $db->query($sql);
+	$num = $stm->rowCount();
+	if ($num == 0) {
+		echo '<p class="error">Non sono presenti clienti per questo studente.</p>';
+	}
+	else {
+		include('include/tabella_clienti.php');
+
+		$form = '<form action="clienti.php" method="post">';
+		$form .= result_as_table($stm, true);
+		$form .= '<input type="submit" name="salva" value="Salva" class="brg" />';
+		$form .= '<input type="hidden" name="matricola" value="'.$matricola.'" />';
+		$form .= '</form>';
+	}
+}
+catch(PDOException $e) {
+        echo $e->getMessage();
+}
+
+$form .= '<form action="aggiungi_evento.php" method="post">';
+$form .= '<input type="submit" name="nuovo_evento" value="Crea Nuovo Evento" class="brg" />';
+$form .= '<input type="hidden" name="submitted" value="TRUE" />';
+$form .= '<input type="hidden" name="matricola" value="'.$matricola.'" />';
+$form .= '</form>';
+
+
+echo $form;
+
+echo '<a href="dettagli_studente.php?matricola=' . $matricola . '">Torna a Dettagli Studente</a>';
+
+include('include/footer.html');
+?>
---
+++ include/tabella_clienti.php
@@ -0,0 +1,25 @@
+<?php
+function result_as_table($result, $tableFeatures="")
+{
+	$table = "<table $tableFeatures>\n\n";
+	$noFields = $result->columnCount();
+	$table .= "<tr>\n";
+	$table .= '<th align="left" style="width:50px;"><b>Default</b></th>';
+	$table .= '<th align="left" style="width:150;">Cognome</th>';
+	$table .= '<th align="left" style="width:150;">Nome</th>';
+	$table .= '<th align="left" style="width:150;">Codice Fiscale</th>';
+	$bg = '#eeeeee';
+	while ($r = $result->fetch(PDO::FETCH_BOTH)) {
+		$bg = ($bg=='#eeeeee' ? '#ffffff' : '#eeeeee');
+		$table .= '<tr bgcolor="' . $bg . '">';
+		$table .= '<td align="center"><input type="checkbox" name="id_persona[]" value="'.$r['id_persona'].'" ' . $r['is_default'] . ' /></td>';
+		$table .= '<td align="left">'.$r['cognome'].'</td>';
+		$table .= '<td align="left">'.$r['nome'].'</td>';
+		$table .= '<td align="left">'.$r['codice_fiscale'].'</td>';
+		$table .= "</tr>\n";
+	}
+	$table .= "</table>\n\n";
+	
+	return $table;
+}
+?>
