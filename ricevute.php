9f684a736f28069a1288e6f9901b980ba7328be8
root <root@pico.intranet.michelerua.it>
2012-01-18 08:59:29 +0100

    20 ricevute per pagina

--- ricevute.php
+++ ricevute.php
@@ -32,7 +32,7 @@ catch(PDOException $e) {
        	echo $e->getMessage();
 }
 
-$display = 10; // 10 records per page
+$display = 20; // 20 records per page
 
 if (isset($_GET['p']) && is_numeric($_GET['p'])) {
 	$pages = $_GET['p'];
